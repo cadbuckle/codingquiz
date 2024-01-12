5da8cfe44e9c26b1cad2ad1c6208115ded0f525f
Duncan B <duncanbuckle@gmail.com>
2024-01-12 13:51:52 +0000

    Questions displaying. Answer validation added. Start of adding timer.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -13,6 +13,7 @@ var questionsEl = document.getElementById("questions");
 var choicesEl = document.getElementById("choices");
 var endScreenEl = document.getElementById("end-screen");
 var timeEl = document.getElementById("time");
+var liEls;
 
 // define variables
 var quizQuestions = [
@@ -48,8 +49,9 @@ var quizQuestions = [
     }
 ];
 var gameTime;
-var timeLeft = 30;
+var timeLeft = 60;
 var currentQuestion = 0;
+var currentCorrectAnswer;
 
 // startTheGame is called when start button clicked
 function startTheGame(event) {
@@ -58,25 +60,39 @@ function startTheGame(event) {
     startButtonEl.disabled = true;
     questionsEl.style = "display: block";
     goTimer();
+    displayQuestion();
 }
 
 // goTimer starts and stops the game timer and calls either gameWon or gameLost accordingly
 function goTimer() {
-    // gameTime = setInterval(function() {
-    //     timeLeft--;
-    //     timeEl.textContent = timeLeft;
-    //     if (timeLeft >= 0) {
-    //         displayQuestion();
-    //     } 
-    //     // Time over?
-    //     if (timeLeft === 0) {
-    //         clearInterval(gameTime);
-    //         gameLost();
-    //     }
-    // }, 1000)
-    displayQuestion();
+    gameTime = setInterval(function() {
+        timeLeft--;
+        timeEl.textContent = timeLeft;
+        // Time over?
+        if (timeLeft === 0) {
+            clearInterval(gameTime);
+            gameEnd();
+        }
+    }, 1000)
 }
 
+// event to validate selected option against actual answer
+function validSelection(event) {
+    // if click target is not the same as correct answer?
+    console.log(event.target.id+" : "+currentCorrectAnswer);
+    if (event.target.id != currentCorrectAnswer) {
+        // incorrect so decrement time left
+        timeLeft -= 10;
+    }
+    // display next question
+    if (currentQuestion >= quizQuestions.length) {
+        gameEnd();
+    } else {
+        displayQuestion();
+    }
+}
+
+// this function displays the question
 function displayQuestion() {
     // get question-title element to display questions
     var questionEl = document.getElementById("question-title");
@@ -85,19 +101,25 @@ function displayQuestion() {
     // build list of possible answers
     var currentChoices = "<ol>";
     questionEl.textContent = currentQuest.question;
-console.log(currentQuest.choices.length);
     for (let i = 0; i < currentQuest.choices.length; i++) {
-        currentChoices += "<li>"+ currentQuest.choices[i]+"</li>";
+        currentChoices += "<li id="+i+">"+ currentQuest.choices[i]+"</li>";
     }
     currentChoices +="</ol>"
-console.log(": "+currentChoices);
     choicesEl.innerHTML = currentChoices;
+    liEls = document.querySelectorAll("li");
+    currentCorrectAnswer = currentQuest.correctAnswer;
     currentQuestion++;
 }
 
-function gameLost() {
-    alert("You lost");
+// gameEnd event
+function gameEnd() {
+    clearInterval(gameTime);
+    questionsEl.style = "display: none";
+    endScreenEl.style = "display: block";
 }
 
 // Listen for the start button to be pressed to call startTheGame function 
 startButtonEl.addEventListener("click", startTheGame);
+
+// add click listen to the list
+choicesEl.addEventListener("click", validSelection);
